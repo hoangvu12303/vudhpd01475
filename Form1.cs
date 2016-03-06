2232fa4c12692b470198881bc540c2053ea640ff
hoangvu12303 <hoangvu78909@gmail.com>
2016-03-06 14:02:03 +0700

    vudhpd01475

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vudhpd01475
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kHACH_HANGBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.kHACH_HANGBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSet1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.KHACH_HANG' table. You can move, or remove it, as needed.
+            this.kHACH_HANGTableAdapter.Fill(this.dataSet1.KHACH_HANG);
+
+        }
+
+        private void kHACH_HANGDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
